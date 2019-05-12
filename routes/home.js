b4c72caa55247cb6354f5900e156c68e81cc91df
Minh17020898 <43349538+Minh17020898@users.noreply.github.com>
2019-05-12 23:04:29 +0700

    Update home.js

--- routes/home.js
+++ routes/home.js
@@ -35,6 +35,24 @@ router.get('(/:status)?', function(req, res, next) {
   pagination.currentPage = parseInt(paramsHelper.getParams(req.query,'page', 1));
 
 //condition for search and filter
+//search
+if(currentStatus === '') {
+  if(keyWord !== '') condition = {name: new RegExp(keyWord, 'i')};
+} else {
+  if(keyWord !== '') condition = {category: currentStatus, name: new RegExp(keyWord, 'i')};
+  else if(keyWord === '') condition = {category: currentStatus};
+}
+
+itemsModel.find(condition).then((items) => {
+  res.render('page/home/index', { 
+    title: 'trang chủ',
+    items,
+    statusFilter,
+    keyWord,
+    currentStatus
+ });
+});
+
 //thong-tin-chi-tiet
 router.get('/thong-tin-chi-tiet/:id', (req, res, next) => {
   let id = paramsHelper.getParams(req.params, 'id', '')
